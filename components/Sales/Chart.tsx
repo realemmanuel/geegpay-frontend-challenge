4cb87ff506a470bd96bab8634568892a33b567c4
Emmanuel Taiwo <devemmanuel1@gmail.com>
2024-01-25 16:53:31 +0100

    chore: built custom chart component

---
+++ components/Sales/Chart.tsx
@@ -0,0 +1,48 @@
+import React, { FC, useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  src: string;
+  height: number;
+  amount: string;
+};
+
+const Chart: FC<Props> = ({ src, height, amount }) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  return (
+    <div className="w-full mt-auto transition-all duration-500 flex flex-col gap-3 items-center">
+      {isHovered && (
+        <>
+          <Image
+            src="/assets/images/amount-box.svg"
+            width={200}
+            height={200}
+            alt="Chart"
+            className="mt-auto min-w-fit"
+          />
+          <span className="text-white mt-[4px] absolute text-[12px] font-[400]">
+            {amount}
+          </span>
+        </>
+      )}
+      <Image
+        style={{
+          background: !isHovered
+            ? "linear-gradient(180deg, rgba(10,249,80,1) 0%, rgba(130,255,116,1) 35%, rgba(186,255,186,0.5704656862745099) 100%)"
+            : "rgb(10, 249, 80)",
+          maxHeight: height,
+          maxWidth: 30,
+        }}
+        src={src}
+        width={30}
+        height={100}
+        alt="Chart"
+        className="dark:filter dark:invert  rounded-t-full transition-all duration-500"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
+    </div>
+  );
+};
+
+export default Chart;
--- components/Sales/SalesTrend.tsx
+++ components/Sales/SalesTrend.tsx
@@ -1,10 +1,10 @@
 import { SALES_AMOUNT, SALES_CHART } from "@/data/sales";
-import Image from "next/image";
 import CalendarDropdown from "./CalendarDropdown";
+import Chart from "./Chart";
 
 const SalesTrend = () => {
   return (
-    <section className="md:mx-5 bg-white dark:bg-gray-600 w-[97%] xl:w-[60%] h-fit md:h-[400px] rounded-[14px] border-[#EDF2F7] dark:border-[#3d6185] border-[1px] flex flex-col gap-5 p-5">
+    <section className="md:mx-5 bg-white dark:bg-gray-600 w-[97%] xl:w-[60%] h-fit md:h-fit lg:h-[420px] rounded-[14px] border-[#EDF2F7] dark:border-[#3d6185] border-[1px] flex flex-col gap-5 p-5">
       <div className="w-full flex flex-col md:flex-row justify-between md:items-center">
         <h3 className="text-[#26282C] dark:text-gray-300 text-[17px] md:text-[20px] font-[400]">
           Sales Trends
@@ -14,7 +14,7 @@ const SalesTrend = () => {
       </div>
 
       <div className="w-full flex items-end gap-8 overflow-auto">
-        <ul className="flex flex-col gap-5">
+        <ul className="flex flex-col gap-7">
           {SALES_AMOUNT.map((item) => (
             <li
               key={item.id}
@@ -25,24 +25,16 @@ const SalesTrend = () => {
           ))}
         </ul>
 
-        <ul className="w-full justify-between mt-auto flex gap-6">
+        <ul className="w-full z-50 justify-between mt-auto flex gap-5">
           {SALES_CHART.map((item) => (
-            <li key={item.id} className="flex flex-col gap-3 items-center">
-              {item.topSale && (
-                <Image
-                  src="/assets/images/top-sale.svg"
-                  width={100}
-                  height={100}
-                  alt="Chart"
-                  className="mt-auto"
-                />
-              )}
-              <Image
+            <li
+              key={item.id}
+              className="w-full transition-all duration-500 flex flex-col gap-3 items-center "
+            >
+              <Chart
                 src={item.chartImg}
-                width={30}
                 height={item.height}
-                alt="Chart"
-                className="mt-auto dark:filter dark:invert"
+                amount={item.amount}
               />
               <span className="text-[14px] text-[#525252] dark:text-gray-400 font-[600]">
                 {item.month}
